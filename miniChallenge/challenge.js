6d27c51f43ed4e4f609e39a13e373b50fcc55b10
SchuylerL08 <schuyler.lupino@vtsd.com>
2022-01-18 11:07:09 -0500

    Javascript

---
+++ miniChallenge/challenge.js
@@ -0,0 +1,9 @@
+/*
+* Design a page that when you change a text input, an image gets bigger.
+*/
+
+function imageBig() {
+    var img = document.getElementById("image");
+    img.style.width = "2000px";
+    img.style.height = "200px"
+}
